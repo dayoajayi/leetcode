96970ea9b8ef10e028cf18c40d7671caf73da224
Dayo Ajayi <dayo.ajayi@gmail.com>
2021-09-20 15:50:40 -0500

    Time: 29 ms (74.41%), Space: 14.2 MB (54.44%) - LeetHub

---
+++ number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.py
@@ -0,0 +1,5 @@
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        s = "".join(c if c.isdigit() else " " for c in word)        
+        return len(set(map(int, s.split())))
+        
