229bf601a360be220d5ad13dd4e6bb96d513abd7
Dayo Ajayi <dayo.ajayi@gmail.com>
2022-01-04 07:44:18 +0300

    Time: 92 ms (10.97%), Space: 14.5 MB (63.01%) - LeetHub

---
+++ 635-design-log-storage-system/635-design-log-storage-system.py
@@ -0,0 +1,24 @@
+class LogSystem:
+
+    def __init__(self):
+        self.logs = []
+        self.g = {"Year": 4, "Month": 7, "Day": 10, "Hour": 13, "Minute": 16, "Second": 19}
+        
+
+    def put(self, id: int, timestamp: str) -> None:
+        self.logs.append([timestamp, id])
+
+    def retrieve(self, start: str, end: str, granularity: str) -> List[int]:
+        idx = self.g[granularity]
+        s,e = start[:idx], end[:idx]
+        return [i for time, i in self.logs if s <= time[:idx] <= e]
+
+
+# 2017:01:01:23:59:59
+# self.g = {"Year": 4, "Month": 7, "Day": 10, "Hour": 13, "Minute": 16, "Second": 19}
+
+        
+# Your LogSystem object will be instantiated and called as such:
+# obj = LogSystem()
+# obj.put(id,timestamp)
+# param_2 = obj.retrieve(start,end,granularity)
