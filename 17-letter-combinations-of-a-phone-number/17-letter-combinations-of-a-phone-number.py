eda2eafeea504b683457cd474f7810cc4db46436
Dayo Ajayi <dayo.ajayi@gmail.com>
2022-01-05 20:50:00 -0600

    Time: 29 ms (66.40%), Space: 14.4 MB (5.29%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,37 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        KEYPAD = {
+            "0": ["0"],
+            "1": ["1"],
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        
+        allCombos = []
+        currentCombo = ["0"] * len(digits)
+                
+        def comboHelper(idx):
+            if idx == len(digits):
+                combo = "".join(currentCombo)
+                allCombos.append(combo)
+            else:
+                digit = digits[idx]
+                letters = KEYPAD[digit]
+                for letter in letters:
+                    currentCombo[idx] = letter
+                    comboHelper(idx + 1)            
+            
+        if digits == "": return []
+        comboHelper(0)
+        return allCombos
+            
+'''
+T: O()
+S: O()
+'''
