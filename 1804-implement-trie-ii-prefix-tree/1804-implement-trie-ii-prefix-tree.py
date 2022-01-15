2b6e4da4c2c0617fd91b84f808e8feb7dab81988
Dayo Ajayi <dayo.ajayi@gmail.com>
2022-01-14 18:31:46 -0600

    Time: 364 ms (87.87%), Space: 28.1 MB (43.61%) - LeetHub

---
+++ 1804-implement-trie-ii-prefix-tree/1804-implement-trie-ii-prefix-tree.py
@@ -0,0 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.prefixCount = 0
+        self.wordCount = 0
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for letter in word:
+            current = current.children[letter]
+            current.prefixCount += 1
+        current.wordCount += 1
+
+    def countWordsEqualTo(self, word: str) -> int:
+        current = self.root
+        for letter in word:
+            if letter not in current.children:
+                return 0
+            else:
+                current = current.children[letter]
+        return current.wordCount
+            
+
+    def countWordsStartingWith(self, prefix: str) -> int:
+        current = self.root
+        for letter in prefix:
+            if letter not in current.children:
+                return 0
+            else:
+                current = current.children[letter]
+        return current.prefixCount
+
+    def erase(self, word: str) -> None:
+        current = self.root
+        for letter in word:
+            current = current.children[letter]
+            current.prefixCount -= 1
+        current.wordCount -= 1
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.countWordsEqualTo(word)
+# param_3 = obj.countWordsStartingWith(prefix)
+# obj.erase(word)
